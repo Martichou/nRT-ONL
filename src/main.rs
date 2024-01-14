ea927a569f608ebca4800904b73fc2a25cd11dcb
Martichou <m@rtin.fyi>
2024-01-14 19:44:37 +0100

    init: base example & add TcpFlag
    Signed-off-by: Martichou <m@rtin.fyi>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,218 @@
+use bitflags::bitflags;
+use pnet::datalink::Channel::Ethernet;
+use pnet::datalink::{self, NetworkInterface};
+use pnet::packet::ethernet::{EtherTypes, EthernetPacket};
+use pnet::packet::icmp::{echo_reply, echo_request, IcmpPacket, IcmpTypes};
+use pnet::packet::icmpv6::Icmpv6Packet;
+use pnet::packet::ip::{IpNextHeaderProtocol, IpNextHeaderProtocols};
+use pnet::packet::ipv4::Ipv4Packet;
+use pnet::packet::ipv6::Ipv6Packet;
+use pnet::packet::tcp::TcpPacket;
+use pnet::packet::Packet;
+use std::{env, net::IpAddr, process};
+
+bitflags! {
+    #[derive(Debug)]
+    struct TcpFlag: u8 {
+        const FIN = 0x01;
+        const SYN = 0x02;
+        const RST = 0x04;
+        const PSH = 0x08;
+        const ACK = 0x10;
+        const URG = 0x20;
+        const ECE = 0x40;
+        const CWR = 0x80;
+    }
+}
+
+fn handle_icmp_packet(interface_name: &str, source: IpAddr, destination: IpAddr, packet: &[u8]) {
+    let icmp_packet = IcmpPacket::new(packet);
+    if let Some(icmp_packet) = icmp_packet {
+        match icmp_packet.get_icmp_type() {
+            IcmpTypes::EchoReply => {
+                let echo_reply_packet = echo_reply::EchoReplyPacket::new(packet).unwrap();
+                println!(
+                    "[{}]: ICMP echo reply {} -> {} (seq={:?}, id={:?})",
+                    interface_name,
+                    source,
+                    destination,
+                    echo_reply_packet.get_sequence_number(),
+                    echo_reply_packet.get_identifier()
+                );
+            }
+            IcmpTypes::EchoRequest => {
+                let echo_request_packet = echo_request::EchoRequestPacket::new(packet).unwrap();
+                println!(
+                    "[{}]: ICMP echo request {} -> {} (seq={:?}, id={:?})",
+                    interface_name,
+                    source,
+                    destination,
+                    echo_request_packet.get_sequence_number(),
+                    echo_request_packet.get_identifier()
+                );
+            }
+            _ => println!(
+                "[{}]: ICMP packet {} -> {} (type={:?})",
+                interface_name,
+                source,
+                destination,
+                icmp_packet.get_icmp_type()
+            ),
+        }
+    } else {
+        println!("[{}]: Malformed ICMP Packet", interface_name);
+    }
+}
+
+fn handle_icmpv6_packet(interface_name: &str, source: IpAddr, destination: IpAddr, packet: &[u8]) {
+    let icmpv6_packet = Icmpv6Packet::new(packet);
+    if let Some(icmpv6_packet) = icmpv6_packet {
+        println!(
+            "[{}]: ICMPv6 packet {} -> {} (type={:?})",
+            interface_name,
+            source,
+            destination,
+            icmpv6_packet.get_icmpv6_type()
+        )
+    } else {
+        println!("[{}]: Malformed ICMPv6 Packet", interface_name);
+    }
+}
+
+fn handle_tcp_packet(interface_name: &str, source: IpAddr, destination: IpAddr, packet: &[u8]) {
+    let tcp = TcpPacket::new(packet);
+    if let Some(tcp) = tcp {
+        println!(
+            "[{}]: TCP Packet({:?} : {}): {}:{} > {}:{}; length: {}",
+            interface_name,
+            TcpFlag::from_bits_truncate(tcp.get_flags()),
+            tcp.get_sequence(),
+            source,
+            tcp.get_source(),
+            destination,
+            tcp.get_destination(),
+            packet.len()
+        );
+    } else {
+        println!("[{}]: Malformed TCP Packet", interface_name);
+    }
+}
+
+fn handle_transport_protocol(
+    interface_name: &str,
+    source: IpAddr,
+    destination: IpAddr,
+    protocol: IpNextHeaderProtocol,
+    packet: &[u8],
+) {
+    match protocol {
+        IpNextHeaderProtocols::Udp => {
+            println!(
+                "[{}]: UDP Packet: {} > {}; length: {}",
+                interface_name,
+                source,
+                destination,
+                packet.len()
+            );
+        }
+        IpNextHeaderProtocols::Tcp => {
+            handle_tcp_packet(interface_name, source, destination, packet)
+        }
+        IpNextHeaderProtocols::Icmp => {
+            handle_icmp_packet(interface_name, source, destination, packet)
+        }
+        IpNextHeaderProtocols::Icmpv6 => {
+            handle_icmpv6_packet(interface_name, source, destination, packet)
+        }
+        _ => println!(
+            "[{}]: UKN {} packet: {} > {}; protocol: {:?} length: {}",
+            interface_name,
+            match source {
+                IpAddr::V4(..) => "IPv4",
+                _ => "IPv6",
+            },
+            source,
+            destination,
+            protocol,
+            packet.len()
+        ),
+    }
+}
+
+fn handle_ipv4_packet(interface_name: &str, ethernet: &EthernetPacket) {
+    let header = Ipv4Packet::new(ethernet.payload());
+    if let Some(header) = header {
+        handle_transport_protocol(
+            interface_name,
+            IpAddr::V4(header.get_source()),
+            IpAddr::V4(header.get_destination()),
+            header.get_next_level_protocol(),
+            header.payload(),
+        );
+    } else {
+        println!("[{}]: Malformed IPv4 Packet", interface_name);
+    }
+}
+
+fn handle_ipv6_packet(interface_name: &str, ethernet: &EthernetPacket) {
+    let header = Ipv6Packet::new(ethernet.payload());
+    if let Some(header) = header {
+        handle_transport_protocol(
+            interface_name,
+            IpAddr::V6(header.get_source()),
+            IpAddr::V6(header.get_destination()),
+            header.get_next_header(),
+            header.payload(),
+        );
+    } else {
+        println!("[{}]: Malformed IPv6 Packet", interface_name);
+    }
+}
+
+fn handle_ethernet_frame(interface: &NetworkInterface, ethernet: &EthernetPacket) {
+    let interface_name = &interface.name[..];
+    match ethernet.get_ethertype() {
+        EtherTypes::Ipv4 => handle_ipv4_packet(interface_name, ethernet),
+        EtherTypes::Ipv6 => handle_ipv6_packet(interface_name, ethernet),
+        _ => println!(
+            "[{}]: UKN packet: {} > {}; ethertype: {:?} length: {}",
+            interface_name,
+            ethernet.get_source(),
+            ethernet.get_destination(),
+            ethernet.get_ethertype(),
+            ethernet.packet().len()
+        ),
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let iface_name = match env::args().nth(1) {
+        Some(n) => n,
+        None => {
+            eprintln!("USAGE: packetdump <NETWORK INTERFACE>");
+            process::exit(1);
+        }
+    };
+    let interface_names_match = |iface: &NetworkInterface| iface.name == iface_name;
+
+    // Find the network interface with the provided name
+    let interfaces = datalink::interfaces();
+    let interface = interfaces
+        .into_iter()
+        .find(interface_names_match)
+        .unwrap_or_else(|| panic!("No such network interface: {}", iface_name));
+
+    // Create a channel to receive on
+    let (_, mut rx) = match datalink::channel(&interface, Default::default()) {
+        Ok(Ethernet(tx, rx)) => (tx, rx),
+        Ok(_) => panic!("packetdump: unhandled channel type"),
+        Err(e) => panic!("packetdump: unable to create channel: {}", e),
+    };
+
+    loop {
+        match rx.next() {
+            Ok(packet) => handle_ethernet_frame(&interface, &EthernetPacket::new(packet).unwrap()),
+            Err(e) => panic!("packetdump: unable to receive packet: {}", e),
+        }
+    }
 }
