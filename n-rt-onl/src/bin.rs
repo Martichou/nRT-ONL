cc24330a9a4b6008ff2741983e1efed87e9bb430
Martichou <m@rtin.fyi>
2024-01-17 22:27:13 +0100

    feat: re-implement library structure
    Signed-off-by: Martichou <m@rtin.fyi>

---
+++ n-rt-onl/src/bin.rs
@@ -0,0 +1,34 @@
+#[macro_use]
+extern crate log;
+
+use n_rt_onl::Onl;
+use std::{env, process};
+
+#[tokio::main]
+
+async fn main() -> Result<(), anyhow::Error> {
+    // Define log level
+    if std::env::var("RUST_LOG").is_err() {
+        std::env::set_var("RUST_LOG", "DEBUG")
+    }
+
+    // Init logger/tracing
+    tracing_subscriber::fmt::init();
+
+    let iface_name = match env::args().nth(1) {
+        Some(n) => n,
+        None => {
+            println!("USAGE: onl <NETWORK INTERFACE>");
+            process::exit(1);
+        }
+    };
+
+    let onl = Onl::new(iface_name, None)?;
+    let mut receiver = onl.start()?;
+
+    while let Some(e) = receiver.recv().await {
+        info!("Got an event: {:?}", e);
+    }
+
+    Ok(())
+}
---
+++ n-rt-onl/src/lib.rs
@@ -0,0 +1,149 @@
+#[macro_use]
+extern crate log;
+
+use anyhow::Context;
+use aya::programs::{Xdp, XdpFlags};
+use aya::{include_bytes_aligned, maps::HashMap, Bpf};
+use aya_log::BpfLogger;
+use pnet::datalink::{self, NetworkInterface};
+use std::io::Error;
+use std::time::Duration;
+use tokio::sync::mpsc::{self, Receiver, Sender};
+
+#[cfg(debug_assertions)]
+static BPF_BYTES: &[u8] =
+    include_bytes_aligned!("../../target/bpfel-unknown-none/debug/n-rt-onl-ebpf");
+#[cfg(not(debug_assertions))]
+static BPF_BYTES: &[u8] =
+    include_bytes_aligned!("../../target/bpfel-unknown-none/release/n-rt-onl-ebpf");
+
+#[derive(Debug, PartialEq)]
+pub enum State {
+    Error,
+    Ukn,
+    Down,
+    Up,
+}
+
+#[derive(Debug, Clone)]
+pub struct Config {
+    /// The MAX time difference in µs between RX/TX packets.
+    /// Default to 1s (1000000µs).
+    rxtx_threshold: u64,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Config {
+            rxtx_threshold: 1000000,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Onl {
+    event_rx: Receiver<State>,
+    event_tx: Sender<State>,
+    iface_name: String,
+    config: Config,
+    bpf: Bpf,
+}
+
+impl Onl {
+    pub fn new(ifname: String, config: Option<Config>) -> Result<Self, anyhow::Error> {
+        // Find the network interface with the provided name
+        let interface = datalink::interfaces()
+            .into_iter()
+            .find(|iface: &NetworkInterface| iface.name == ifname);
+
+        if interface.is_none() {
+            return Err(Error::new(
+                std::io::ErrorKind::NotFound,
+                format!("iface({}) not found, check name and permissions", ifname),
+            )
+            .into());
+        }
+
+        // Bump the memlock rlimit. This is needed for older kernels that don't use the
+        // new memcg based accounting, see https://lwn.net/Articles/837122/
+        let rlim = libc::rlimit {
+            rlim_cur: libc::RLIM_INFINITY,
+            rlim_max: libc::RLIM_INFINITY,
+        };
+        let ret = unsafe { libc::setrlimit(libc::RLIMIT_MEMLOCK, &rlim) };
+        if ret != 0 {
+            debug!("remove limit on locked memory failed, ret is: {}", ret);
+        }
+
+        let channel = mpsc::channel(100);
+
+        Ok(Self {
+            event_tx: channel.0,
+            event_rx: channel.1,
+            iface_name: ifname,
+            config: config.unwrap_or_default(),
+            bpf: Bpf::load(BPF_BYTES).unwrap(),
+        })
+    }
+
+    /// Start the outage notification process.
+    /// Returning the receiver of a MPSC channel.
+    pub fn start(mut self) -> Result<Receiver<State>, anyhow::Error> {
+        if let Err(e) = BpfLogger::init(&mut self.bpf) {
+            // This can happen if you remove all log statements from your eBPF program.
+            warn!("failed to initialize eBPF logger: {}", e);
+        }
+
+        let program: &mut Xdp = self.bpf.program_mut("n_rt_onl_ebpf").unwrap().try_into()?;
+        program.load()?;
+        program.attach(&self.iface_name, XdpFlags::default())
+			.context("failed to attach the XDP program with default flags - try changing XdpFlags::default() to XdpFlags::SKB_MODE")?;
+
+        tokio::spawn(async move {
+            let bpf_map = self.bpf.map_mut("PKT_TIMESTAMP").unwrap();
+            let pkt_timestamp = HashMap::<_, u8, u64>::try_from(bpf_map).unwrap();
+
+            // Need some inner state to know if we're in an "outage" or not
+            let mut current = State::Ukn;
+
+            loop {
+                let start_overall = std::time::Instant::now();
+
+                let rx_pkt = pkt_timestamp.get(&0, 0).unwrap_or_default();
+                let tx_pkt = pkt_timestamp.get(&1, 0).unwrap_or_default();
+                let abs_diff = rx_pkt.abs_diff(tx_pkt);
+
+                debug!("Running check: {} <= {} - {}", abs_diff, rx_pkt, tx_pkt);
+                match current {
+                    State::Up | State::Ukn => {
+                        // If the diff is bigger than 1s
+                        if abs_diff > self.config.rxtx_threshold {
+                            info!("State now DOWN");
+                            _ = self.event_tx.send(State::Down).await;
+                            current = State::Down;
+                        }
+                    }
+                    State::Down => {
+                        if abs_diff < self.config.rxtx_threshold {
+                            info!("State now UP");
+                            _ = self.event_tx.send(State::Up).await;
+                            current = State::Up;
+                        }
+                    }
+                    _ => {}
+                }
+
+                // If the state is still Ukn, this means we're Up.
+                if current == State::Ukn {
+                    _ = self.event_tx.send(State::Up).await;
+                    current = State::Up;
+                }
+
+                let duration_overall = start_overall.elapsed();
+                std::thread::sleep(Duration::from_secs(1) - duration_overall);
+            }
+        });
+
+        Ok(self.event_rx)
+    }
+}
